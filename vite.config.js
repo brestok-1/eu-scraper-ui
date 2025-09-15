a87f5c0907009189b1cdca50cb437f91336e949b
brestok <6234513@gmail.com>
2025-09-15 20:15:56 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: 'https://brestok-1.github.io/eu-scraper-ui',
   plugins: [react(), tailwind()],
 })
