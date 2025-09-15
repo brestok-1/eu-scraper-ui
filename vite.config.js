3f38ee6a705d4265123da8fcb8070389259396cc
brestok <6234513@gmail.com>
2025-09-15 20:13:30 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: 'https://brestok-eu-scrapper-ui.static.hf.space/index.html',
   plugins: [react(), tailwind()],
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'build',
-    emptyOutDir: true,
-  },
 })
-
-
